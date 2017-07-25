922fafe2ec8d00dcaa44465dbcf9e5c85056d835
mjtakenon <takenon777@gmail.com>
2017-07-25 15:58:59 +0900

    add bar color

--- KinectarGame/NotesManager.cpp
+++ KinectarGame/NotesManager.cpp
@@ -1,8 +1,9 @@
 #include "NotesManager.h"
 
-NotesManager::NotesManager(list<Note> notes, Bar bar, int samplingRate)
+NotesManager::NotesManager(list<Note> notes, vector<Bar> bars, int samplingRate)
 {
 	m_Notes = notes;
+	m_Bars = bars;
 	m_SamplingRate = samplingRate;
 
 	double prepareTime = 5;
@@ -17,12 +18,11 @@ NotesManager::NotesManager(list<Note> notes, Bar bar, int samplingRate)
 	
 	for (auto itr = m_Notes.begin(); itr != m_Notes.end(); itr++)
 	{
-		itr->setSize(Vec2(10, 10));
-		itr->setSpeed(Vec2(10, 0));
-		itr->setPosition(Vec2(bar.getPosition().x + m_PrepareFrame*itr->getSpeed().x , itr->getString() * 20 + 100));
+		itr->setSize(Vec2(20, 15));
+		itr->setSpeed(Vec2(5, 0));
+
+		itr->setPosition(Vec2(m_Bars[itr->getString()].getPosition().x + m_PrepareFrame*itr->getSpeed().x , m_Bars[itr->getString()].getPosition().y));
 	}
-
-
 }
 
 NotesManager::~NotesManager()
@@ -30,16 +30,15 @@ NotesManager::~NotesManager()
 
 }
 
-void NotesManager::Update(pair<vector<int>,vector<int>> input, int sample, Bar bar)
+void NotesManager::Update(pair<vector<int>,vector<int>> input, int sample)
 {
-	//inputからNotesの探索、適切なノーツの削除、新しいノーツの出現とか 
-	//音楽とずれてたら調整とかも?→Game.cppの方でやらせるべきかな ←解決?
+	//inputからNotesの探索、適切なノーツの削除、新しいノーツの出現とか
 	for (auto itr = m_Notes.begin(); itr != m_Notes.end();)
 	{
 		//出現と移動
-		itr->Update(sample, m_SamplingRate, bar.getPosition(), m_HitSample, m_VisibleSample);
+		itr->Update(sample, m_SamplingRate, m_Bars[itr->getString()].getPosition(), m_HitSample, m_VisibleSample);
 
-		//listの要素削除、面倒ですね
+		//listの要素削除
 		if (itr->getPosition().x <= 0 && itr->isVisible())
 		{
 			itr = m_Notes.erase(itr);
@@ -47,8 +46,6 @@ void NotesManager::Update(pair<vector<int>,vector<int>> input, int sample, Bar b
 		}
 		itr++;
 	}
-
-
 }
 
 void NotesManager::Draw()
@@ -56,6 +53,5 @@ void NotesManager::Draw()
 	for (auto itr = m_Notes.begin(); itr != m_Notes.end(); itr++)
 	{
 		itr->Draw();
-		Println(itr->getPosition());
 	}
 }
